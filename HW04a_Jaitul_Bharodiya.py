c8b172be330c47a80072b12a2c1a5826f06a716f
jaitul25 <57580916+jaitul25@users.noreply.github.com>
2020-02-16 15:02:14 -0500

    Add files via upload

---
+++ HW04a_Jaitul_Bharodiya.py
@@ -0,0 +1,43 @@
+"""Code to fetch the repository and commits"""
+
+import requests
+import json
+
+
+def get_repo_details(user_name):
+    """Fetched the repository"""
+    result = []
+
+    user_url = f'https://api.github.com/users/{user_name}/repos'
+    
+    result.append(f'User: {user_name}')
+
+    try:
+        res = requests.get(user_url)
+    except (TypeError, KeyError, IndexError):
+        return "unable to fetch user's repos"
+
+    repos = json.loads(res.text)
+
+    try:
+        for repo in repos:
+            repo_name = repo['name']
+            repo_url = 'https://api.github.com/repos/{}/{}/commits'.format(user_name, repo_name)
+            repo_detail = requests.get(repo_url)
+            repo_detail_json = json.loads(repo_detail.text)
+            result.append(f'Repo: {repo_name} Number of commits: {len(repo_detail_json)}')
+    except (TypeError, KeyError, IndexError):
+        return "unable to fetch user's commits"
+
+    return result
+
+def main():
+    """Takes user input and calls the main funct"""
+    usr_name = input("Enter name of the user = ")
+
+    for repo in get_repo_details(usr_name):
+        print(repo)
+
+
+if __name__ == '__main__':
+    main()
---
+++ HW04a_Jaitul_Bharodiya_Test.py
@@ -0,0 +1,14 @@
+"""Test cases for fetching repository"""
+
+import unittest
+from HW04a_Jaitul_Bharodiya import get_repo_details
+
+class TestRepo(unittest.TestCase):
+    """Test the Repos"""
+    def test_repos(self):
+        """Testing the repository and commits"""
+        self.assertEqual(get_repo_details('themalavshastri'),['User: themalavshastri','Repo: Algorithms- Number of commits: 2','Repo: MachineLearning Number of commits: 1'])
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False,verbosity=2)
