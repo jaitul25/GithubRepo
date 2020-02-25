63d1a3a541d2c84b986c6b43249d89ec82f0db0f
jaitul25 <57580916+jaitul25@users.noreply.github.com>
2020-02-25 15:10:34 -0500

    Update HW04a_Jaitul_Bharodiya_Test.py

--- HW04a_Jaitul_Bharodiya_Test.py
+++ HW04a_Jaitul_Bharodiya_Test.py
@@ -1,13 +1,35 @@
 """Test cases for fetching repository"""
 
 import unittest
+import json
+from unittest import mock
 from HW04a_Jaitul_Bharodiya import get_repo_details
+
+
+def get_url(url):
+    if url == "https://api.github.com/users/themalavshastri/repos":
+        return fetch_repo('rep.json')
+
+class content:
+    content = ''
+    
+def fetch_repo(loc):
+    data = content()
+    with open(loc,'r') as f:
+        data.content = json.loads(f)
+    return data.content
+
 
 class TestRepo(unittest.TestCase):
     """Test the Repos"""
-    def test_repos(self):
+
+    @mock.patch('requests.get')
+    def test_repos(self,mocked):
         """Testing the repository and commits"""
-        self.assertEqual(get_repo_details('themalavshastri'),['User: themalavshastri','Repo: Algorithms- Number of commits: 2','Repo: MachineLearning Number of commits: 1'])
+        mocked.side_effect = get_url
+        reposit = get_repo_details('themalavshastri')
+        self.assertEqual(reposit,'unable to fetch users repos')
+
 
 
 if __name__ == '__main__':
